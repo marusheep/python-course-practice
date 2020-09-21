bcd9e104806da3b9c2676045e50af9b5836e11a0
marusheep <38782965+marusheep@users.noreply.github.com>
2020-09-21 16:35:09 +0100

    update

--- course-6-introduction-to-loops/6-9-factorial.py
+++ course-6-introduction-to-loops/6-9-factorial.py
@@ -9,7 +9,7 @@ def factorial(fac_num):
     returned = 1
     for item in range(fac_num,1,-1):
         returned *= item
-    return returned
+    return returned     
 
 print(factorial(3))
 print(factorial(4))
